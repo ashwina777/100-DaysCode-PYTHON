31887b802cedbd0426bcc46822b233209e7d9ec8
ASHWINA <114324721+ashwina777@users.noreply.github.com>
2024-01-26 09:08:50 +0530

    Add files via upload

---
+++ 03_Day3-modules&pip/06_Day3_modules&pip.py
@@ -0,0 +1,20 @@
+import pandas
+""" import any library only after it's installation through command prompt
+    pip install pandas
+    import sklearn #machine learning library
+        import sklearn #machine learning library
+        ^^^^^^^^^^^^^^
+    ModuleNotFoundError: No module named 'sklearn' 
+"""
+import hashlib #built in module no need of installation
+print("hi")
+
+
+"""  -> can run python in terminal(vs code used here) or simply type python in command prompt
+PS C:\Users\Amit rana\Documents\Data Science> python
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print("hello world")
+hello world
+>>>
+"""
