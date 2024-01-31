97c120f6988efcb095bf6bcb407b730ce1237f65
ASHWINA <114324721+ashwina777@users.noreply.github.com>
2024-01-31 15:40:52 +0530

    Add files via upload

---
+++ 08_Day8-exercise1-Solution/11_Day8_Exercise1Sol.py
@@ -0,0 +1,13 @@
+'''
+Exercise 1 - Create a Calculator
+Create a calculator capable of performing addition, subtraction,
+ multiplication and division operations on two numbers. 
+ Your program should format the output in a readable manner!
+'''
+a = 50
+b = 3
+
+print("The value of", a, "+", 3, "is: ", a + b)
+print("The value of", a, "-", 3, "is: ", a - b)
+print("The value of", a, "*", 3, "is: ", a * b)
+print("The value of", a, "/", 3, "is: ", a / b)
