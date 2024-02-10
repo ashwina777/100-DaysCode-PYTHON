d2d9847db2254ea12f7cc358af3e871143749e2f
ASHWINA <114324721+ashwina777@users.noreply.github.com>
2024-02-10 20:40:41 +0530

    Add files via upload

---
+++ 19_Day19-break-and-continue/24_Day19_BreakContinue.py
@@ -0,0 +1,9 @@
+# table
+for i in range(12):
+    if(i==10):
+        break # exit loop
+    if(i==5):
+        print("skip the iteration")
+        continue # skip particular iteration
+    print("5 X ",i+1," = ",5*(i+1))
+    
