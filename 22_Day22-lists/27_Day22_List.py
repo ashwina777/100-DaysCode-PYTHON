8514ea0779092415364007a29d0ac474cbe7b13e
ASHWINA <114324721+ashwina777@users.noreply.github.com>
2024-02-14 20:57:41 +0530

    Update 27_Day22_List.py

--- 22_Day22-lists/27_Day22_List.py
+++ 22_Day22-lists/27_Day22_List.py
@@ -1,32 +1,32 @@
 # list are ordered collection of data items
 # they store multiple items in a single variable
 l=[3,2.5,5,[2,3,4],"ashwina",True]
-# print(l)
-# print(type(l))
-# print(l[0])
-# print(l[2])
-# print(l[3])
-# print(l[3][0])
-# print(l[3][2])
-# print(l[4])
-# # print(l[4]) -> IndexError: list index out of range
+print(l)
+print(type(l))
+print(l[0])
+print(l[2])
+print(l[3])
+print(l[3][0])
+print(l[3][2])
+print(l[4])
+# print(l[4]) -> IndexError: list index out of range
 
-# # negative indexing
-# print(l[-3]) # third last element -> len(l)-3
-# print(l[len(l)-3])
-# print(l[6-3])
+# negative indexing
+print(l[-3]) # third last element -> len(l)-3
+print(l[len(l)-3])
+print(l[6-3])
 
-# if "ashwina" in l:
-#     print("yes")
-# else:
-#     print("no")
-# #same applies for strings as well
-# if "ashwi" in "ashwina rawat":
-#     print("yes")
+if "ashwina" in l:
+    print("yes")
+else:
+    print("no")
+#same applies for strings as well
+if "ashwi" in "ashwina rawat":
+    print("yes")
 print(l[:]) #python automatically puts-> len[0:len(l)] 
 print(l[:1]) # python -> l[0:1]
 print(l[1:-1])
-print(l[1:4]) #slicing-> [start:end:jump]
+print(l[1:4]) #slicing-> list[start:end:jump]
 print(l[0:5:2])
 
 #list comprehension
